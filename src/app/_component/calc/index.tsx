4d0c3857dc84abbe1904f6a89834686d98c9d6be
nishiura-hiroki <nishiurahiroki@gmail.com>
2023-11-30 19:21:30 +0900

    Use form status & Add loading.

--- src/app/_component/calc/index.tsx
+++ src/app/_component/calc/index.tsx
@@ -3,7 +3,102 @@
 import { calcAction } from "./calc.action";
 import styles from "./index.module.css";
 
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
+
+function InputDisplay({ value }: { value: string }) {
+  const state = useFormStatus();
+
+  return (
+    <div className={styles.calculator}>
+      <input
+        type="text"
+        className={styles.display}
+        value={state.pending ? "..." : value}
+        readOnly
+      />
+      <div className={styles.buttons}>
+        <button type="submit" name="action" value="1" className={styles.number}>
+          1
+        </button>
+        <button type="submit" name="action" value="2" className={styles.number}>
+          2
+        </button>
+        <button type="submit" name="action" value="3" className={styles.number}>
+          3
+        </button>
+        <button type="submit" name="action" value="4" className={styles.number}>
+          4
+        </button>
+        <button type="submit" name="action" value="5" className={styles.number}>
+          5
+        </button>
+        <button type="submit" name="action" value="6" className={styles.number}>
+          6
+        </button>
+        <button type="submit" name="action" value="7" className={styles.number}>
+          7
+        </button>
+        <button type="submit" name="action" value="8" className={styles.number}>
+          8
+        </button>
+        <button type="submit" name="action" value="9" className={styles.number}>
+          9
+        </button>
+        <button type="submit" name="action" value="0" className={styles.number}>
+          0
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="+"
+          className={styles.operator}
+        >
+          +
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="-"
+          className={styles.operator}
+        >
+          -
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="*"
+          className={styles.operator}
+        >
+          *
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="/"
+          className={styles.operator}
+        >
+          /
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="clear"
+          className={styles.clear}
+        >
+          C
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="calc"
+          className={styles.equal}
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export function Calc() {
   const initialState = {
@@ -14,144 +109,7 @@ export function Calc() {
 
   return (
     <form action={dispatch}>
-      <div className={styles.calculator}>
-        <input
-          type="text"
-          className={styles.display}
-          value={state?.value}
-          readOnly
-        />
-        <div className={styles.buttons}>
-          <button
-            type="submit"
-            name="action"
-            value="1"
-            className={styles.number}
-          >
-            1
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="2"
-            className={styles.number}
-          >
-            2
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="3"
-            className={styles.number}
-          >
-            3
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="4"
-            className={styles.number}
-          >
-            4
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="5"
-            className={styles.number}
-          >
-            5
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="6"
-            className={styles.number}
-          >
-            6
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="7"
-            className={styles.number}
-          >
-            7
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="8"
-            className={styles.number}
-          >
-            8
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="9"
-            className={styles.number}
-          >
-            9
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="0"
-            className={styles.number}
-          >
-            0
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="+"
-            className={styles.operator}
-          >
-            +
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="-"
-            className={styles.operator}
-          >
-            -
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="*"
-            className={styles.operator}
-          >
-            *
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="/"
-            className={styles.operator}
-          >
-            /
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="clear"
-            className={styles.clear}
-          >
-            C
-          </button>
-          <button
-            type="submit"
-            name="action"
-            value="calc"
-            className={styles.equal}
-          >
-            =
-          </button>
-        </div>
-      </div>
+      <InputDisplay value={state?.value} />
     </form>
   );
 }
